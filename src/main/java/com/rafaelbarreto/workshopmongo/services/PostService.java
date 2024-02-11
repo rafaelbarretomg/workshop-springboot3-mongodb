28539c2af798c4dc9396be8dfe6b9cebcd66d946
Rafael Barreto <rafaelbarretomg@gmail.com>
2024-02-11 20:07:36 -0300

    Obtendo um post por ID

---
+++ src/main/java/com/rafaelbarreto/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.rafaelbarreto.workshopmongo.services;
+
+import com.rafaelbarreto.workshopmongo.domain.Post;
+import com.rafaelbarreto.workshopmongo.domain.User;
+import com.rafaelbarreto.workshopmongo.repository.PostRepository;
+import com.rafaelbarreto.workshopmongo.repository.UserRepository;
+import com.rafaelbarreto.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    PostRepository repo;
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
+    }
+
+}
